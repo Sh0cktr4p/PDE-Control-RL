25d9eb9b4f856b3b2b8ae156d8193f98bbe0e862
Felix Trost <ga94zux@mytum.de>
2021-03-30 14:15:43 +0200

    removed plot tools again, added utility file containing functions for extracting forces from frames

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import numpy as np
+import phi.flow as phiflow
+
+
+def infer_forces_from_frames(frames, domain, diffusion_substeps, viscosity, dt):
+    frames = np.array(frames)
+    step_count = frames.shape[0]
+
+    b = phiflow.Burgers(diffusion_substeps=diffusion_substeps)
+    to_state = lambda v: phiflow.BurgersVelocity(domain, velocity=v, viscosity=viscosity)
+
+    # Simulate all timesteps of all trajectories at once
+    # => concatenate all frames in batch dimension
+    prv = to_state(frames[:-1].reshape((-1,) + frames.shape[2:]))
+    prv_sim = b.step(prv, dt=dt)
+    
+    forces = (frames[1:] - prv_sim.velocity.data.reshape(step_count, -1, *frames.shape[2:])) / dt
+    
+    # Sanity check, should be able to reconstruct goal state with forces
+    s = to_state(frames[0])
+    for i in range(step_count):
+        f = forces[i].reshape(s.velocity.data.shape)
+        effect = phiflow.FieldEffect(CenteredGrid(f, box=domain.box), ['velocity'])
+        s = b.step(s, dt, (effect,))
+    diff = frames[-1] - s.velocity.data
+    print('Maximum deviation from target state: %f' % np.abs(diff).max())
+    return forces
+
+def infer_forces_sum_from_frames(frames, domain, diffusion_substeps, viscosity, dt):
+    forces = infer_forces_from_frames(frames, domain, diffusion_substeps, viscosity, dt)
+    # Sum along frame and field dimensions, keep batch dimension
+    return np.abs(forces).sum(axis=(0, 2)).squeeze()
